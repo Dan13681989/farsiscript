60e39d2f0f9f696e76aa849f09ac0ef94c14c66c
Dan13681989 <139562189+Dan13681989@users.noreply.github.com>
2026-06-23 00:17:07 +0330

    Rename to farsiscript-lang for PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='farsiscript',
+    name='farsiscript-lang',
     version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
